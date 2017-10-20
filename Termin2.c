4fe4cd86086b267b0badaeb3e716cff2b2e0b5a7
Sofian Beyer <caot.50f14n@hotmail.com>
2017-10-20 14:24:46 +0200

    Tried timer into Termin2

--- Termin2.c
+++ Termin2.c
@@ -7,13 +7,17 @@
 #include "../h/pmc.h"
 #include "../h/pio.h"
 #include "../h/aic.h"
+#include "../h/tc.h"
 
 void interrupt (void) __attribute__ ((interrupt));
+void timedInterrupt(void) __attribute__((interrupt));
 
 void interrupt (void)
 {
     StructPIO* piobaseB = PIOB_BASE;	// Basisadresse PIOB
     StructAIC* aicbase = AIC_BASE;        //Basisadresse AIC - advanced interrupt controller
+    
+     aicbase->AIC_EOICR = piobaseB->PIO_ISR; //Zurücksetzen des Interrupts
     
     if (~piobaseB->PIO_PDSR & KEY1)
         piobaseB->PIO_CODR = LED2;
@@ -22,6 +26,46 @@ void interrupt (void)
     
   //  aicbase->AIC_EOICR = piobaseB->PIO_ISR;     //Reset des /EndofInterruptCommandRegister/ mittels schreiben   -   Reset des /InterruptStatusRegister/ durch lesen 
 }
+
+void timedInterrupt(void){
+
+	StructPIO* piobaseB   = PIOB_BASE;
+	StructAIC* aicbase = AIC_BASE;
+	
+    if(~piobaseB->PIO_PDSR & LED3)
+        piobaseB->PIO_CODR = LED3;
+    else if(piobaseB->PIO_PDSR & (~LED3))
+        piobaseB->PIO_SODR = LED3;
+	
+	aicbase->AIC_EOICR = piobaseB->PIO_ISR; 
+
+}
+
+
+
+void Timer3_init( void )
+{
+  StructTC* timerbase3  = TCB3_BASE;		// Basisadressse TC Block 1
+  StructPIO* piobaseA   = PIOA_BASE;		// Basisadresse PIO B
+
+	timerbase3->TC_CCR = TC_CLKDIS;			// Disable Clock
+ 
+  // Initialize the mode of the timer 3
+  timerbase3->TC_CMR = TC_ACPC_CLEAR_OUTPUT  |     TC_ACPA_SET_OUTPUT    |     TC_WAVE               |        TC_CPCTRG             |     TC_CLKS_MCK1024;           //TCCLKS  : MCKI / 1024
+
+  // Initialize the counter:
+  timerbase3->TC_RA = 300;	//__
+  timerbase3->TC_RC = 600;	//__
+
+  // Start the timer :
+  timerbase3->TC_CCR = TC_CLKEN ;				//__
+  timerbase3->TC_CCR = TC_SWTRG ;				//__
+  piobaseA->PIO_PDR  = (1<<PIOTIOA3) ;	//__
+  piobaseA->PIO_OER  = (1<<PIOTIOA3) ;	//__
+  piobaseA->PIO_CODR = (1<<PIOTIOA3) ;	//__
+}
+
+
 
 int main (){
     
@@ -34,17 +78,19 @@ int main (){
     aicbase->AIC_IDCR = 1 << 14;          //Interrupt f?r PIOB ausschalten
     aicbase->AIC_ICCR = 1 << 14;          //Interrupt f?r PIOB l?schen
     aicbase->AIC_SVR[14]= (unsigned int)interrupt;
+    aicbase->AIC_SVR[9] = (unsigned int)timedInterrupt;
+
     aicbase->AIC_SMR[PIOB_ID] = 0x7; // Priorit?t	
     aicbase->AIC_IECR = 1 <<14;           // Interrrupt f?r PIOB aktivieren
     
-    piobaseB->PIO_PER = LED1 | LED2 | KEY1 | KEY2 ;
-    piobaseB->PIO_OER = LED1 | LED2;
+    piobaseB->PIO_PER = LED1 | LED2 | LED3 | KEY1 | KEY2 ;
+    piobaseB->PIO_OER = LED1 | LED2 | LED3;
     piobaseB->PIO_ODR = KEY1 | KEY2 ;
     piobaseB->PIO_IER = KEY1 | KEY2 ;
     
-    piobaseB->PIO_SODR = LED1 | LED2;
+    piobaseB->PIO_SODR = LED1 | LED2 | LED3;
     
-    
+    Timer3_init();
 
     
     while(1)
